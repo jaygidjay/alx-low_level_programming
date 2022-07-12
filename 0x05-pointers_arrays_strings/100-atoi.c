9fe0718e423fb8da32d5e0548faa9ad25e737259
jaygidjay <jimnat.org@gmail.com>
2022-07-12 16:11:58 -0700

    update task10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,13 +10,12 @@ int _atoi(char *s)
 	int sign;
 	unsigned int num;
 	char *temp;
-	
 	temp = s;
 	num = 0;
 	sign = 1;
 	while (*temp != '\0' && (*temp < '0' || *temp > '9'))
 	{
-		if(*temp == '-')
+		if (*temp == '-')
 			sign *= -1;
 		temp++;
 	}
