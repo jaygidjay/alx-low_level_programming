b8578a313982ceb5287a1d3cdfe58fac309e0f16
jaygidjay <107034244+jaygidjay@users.noreply.github.com>
2022-07-17 05:05:23 -0700

    Update 4-rev_array.c

--- 4-rev_array.c
+++ 4-rev_array.c
@@ -1,21 +1,22 @@
 #include "main.h"
 
 /**
- *reverse_array - reverse an array.
- *@a: array.
- *@n: array's length.
+ * reverse_array - reverses the contents of an array on intergers
+ * @a: destination
+ * @n: number of elements in an array
+ * Return: 0
  */
-
 void reverse_array(int *a, int n)
 {
-int i, j, tmp;
+	int i, j, temp;
 
-j = n - 1;
-for (i = 0; i < n / 2; i++)
-{
-tmp = a[i];
-a[i] = a[j];
-a[j] = tmp;
-j--;
-}
+	for (i = 0; i < n - 1; i++)
+	{
+		for (j = i + 1; j > 0; j--)
+		{
+			temp = *(a + j);
+			*(a + j) = *(a + (j - 1));
+			*(a + (j - 1)) = temp;
+		}
+	}
 }
