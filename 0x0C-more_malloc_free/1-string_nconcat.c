c51ff69508ef8c6839a215f2fcc0e19d8b24aca1
jaygidjay <107034244+jaygidjay@users.noreply.github.com>
2022-07-28 14:02:16 -0700

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,48 +1,53 @@
-#include "main.h"
-#include <stdio.h>
+include <stdlib.h>
 #include <stdlib.h>
-
+#include "main.h"
 /**
- *string_nconcat - concat 2 string
- *@s1: string 1
- *@s2: string 2
- *@n: integer value
- *Return: pointer points to a new allocated space else NULL
+ * _strlen - calculate and return string length
+ * @string: string
+ * Return: string length
  */
+int _strlen(char *string)
+{
+	int i;
 
+	for (i = 0; string[i] != '\0'; i++)
+		;
+	return (i);
+}
+/**
+ * string_nconcat - concatenate s1 and n bytes of s2; return ptr to string
+ * @s1: string 1
+ * @s2: string 2
+ * @n: n bytes to concat from string 2
+ * Return: pointer to concatenated string
+ */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-char *str;
-unsigned int s1_len, s2_len, i;
+	char *ptr;
+	int num, len, i, j;
 
+	num = n;
 
-if (s1 == NULL)
-s1 = "";
-if (s2 == NULL)
-s2 = "";
+	if (s1 == NULL) /* account for NULL strings */
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	if (num < 0) /* account for negative n bytes */
+		return (NULL);
+	if (num >= _strlen(s2)) /* account for n too big */
+		num = _strlen(s2);
 
-for (s1_len = 0; s1[s1_len] != '\0'; s1_len++)
-;
+	len = _strlen(s1) + num + 1; /* +1 to account for null pointer */
 
-for (s2_len = 0; s2[s2_len] != '\0'; s2_len++)
-;
+	ptr = malloc(sizeof(*ptr) * len); /* malloc and check for error */
+	if (ptr == NULL)
+		return (NULL);
 
-str = malloc(s1_len + s2_len + 1);
+	for (i = 0; s1[i] != '\0'; i++) /* concat */
+		ptr[i] = s1[i];
+	for (j = 0; j < num; j++)
+		ptr[i + j] = s2[j];
+	ptr[i + j] = '\0';
 
-
-if (n >= s2_len)
-n = s2_len;
-
-for (i = 0; s1[i] != '\0'; i++)
-str[i] = s1[i];
-
-for (i = 0; i < n; i++)
-str[s1_len + i] = s2[i];
-
-str[s1_len + n] = '\0';
-
-if (str == NULL)
-return (NULL);
-
-return (str);
+	return (ptr);
 }
